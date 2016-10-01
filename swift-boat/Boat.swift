bd27364f1f454a014ff7cc2e2058ec8b94abbea3
mariejinpark <mariejinpark@gmail.com>
2016-10-01 12:47:47 -0400

    Boat Lab

---
+++ swift-boat/Boat.swift
@@ -0,0 +1,26 @@
+//
+//  Boat.swift
+//  swift-boat
+//
+//  Created by Marie Park on 10/1/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Boat {
+    let name: String
+    var sailors: [String] = []
+    var maxSpeedKnots: Double
+    var speedKnots: Double = 0.0
+    
+    init(name: String, sailors: [String], maxSpeedKnots: Double) {
+        self.name = name
+        self.sailors = sailors
+        self.maxSpeedKnots = maxSpeedKnots
+    }
+    
+    convenience init (name: String, maxSpeedKnots: Double) {
+        self.init(name: name, sailors: [], maxSpeedKnots: maxSpeedKnots)
+    }
+}
